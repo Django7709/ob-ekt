1dd5f9d7496927f28fb1794a08c8e4b7428763a8
django <pitbull.09@mail.ru>
2024-09-21 07:32:59 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,37 @@ public class Author {
         this.name = name;
         this.lastName = lastName;
     }
-
     public String getName() {
         return this.name;
     }
-
     public String getLastName() {
         return this.lastName;
     }
 }
+public class Book {
+    private String nameBooks;
+    private int agePublication;
+    private String author;
 
+    public Book(String nameBooks, String author, int agePublication) {
+
+        this.nameBooks = nameBooks;
+        this.agePublication = agePublication;
+        this.author = author;
+    }
+    public int getAgePublication() {
+        return this.agePublication;
+    }
+    public String getNameBook() {
+        return this.nameBooks;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
+    public void setAgePublication(int agePublication) {
+        this.agePublication = agePublication;
+    }
+}
 public class Main {
     public static void main(String[] args) {
         printSeparator();
